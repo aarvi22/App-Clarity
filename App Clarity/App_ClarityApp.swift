369edf70c2bae8633863f8feeddfa81c8bf10d3c
Aarvi <aarvishah2023@gmail.com>
2023-08-02 15:11:57 -0500

    Initial Commit

---
+++ App Clarity/App_ClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ClarityApp.swift
+//  App Clarity
+//
+//  Created by Aarvi Shah on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct App_ClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
